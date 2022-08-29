6ab126433cdae66c5309be963449d7d13646ab44
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2022-08-30 00:25:54 +0200

    tweaking with ParticleEngine

--- code.js
+++ code.js
@@ -4,6 +4,27 @@
 
 var fov = {fovValue: 60};
 
+// var candle = {
+//     positionStyle  : Type.SPHERE,
+//     positionBase   : new THREE.Vector3( 0, 50, 0 ),
+//     positionRadius : 2,
+    
+//     velocityStyle  : Type.CUBE,
+//     velocityBase   : new THREE.Vector3(0,100,0),
+//     velocitySpread : new THREE.Vector3(20,0,20),
+    
+//     particleTexture : THREE.ImageUtils.loadTexture( 'images/smokeparticle.png' ),
+    
+//     sizeTween    : new Tween( [0, 0.3, 1.2], [20, 150, 1] ),
+//     opacityTween : new Tween( [0.9, 1.5], [1, 0] ),
+//     colorTween   : new Tween( [0.5, 1.0], [ new THREE.Vector3(0.02, 1, 0.5), new THREE.Vector3(0.05, 1, 0) ] ),
+//     blendStyle : THREE.AdditiveBlending,  
+    
+//     particlesPerSecond : 60,
+//     particleDeathAge   : 1.5,		
+//     emitterDeathAge    : 60
+// }
+
 /////////////////////////////////////
 // ALL THE IMPORTANT FUNCTIONS
 //
@@ -31,8 +52,6 @@ function main(){
 
     {
         // adding background
-
-        // TODO: all in one array??
 
         let materialArray = [];
         let backgroundFront = new THREE.TextureLoader().load('/res/night_sky_Front.jpg');
@@ -120,6 +139,8 @@ function main(){
     ///////////////////////////////////
 
     // TODO: create particle effect - flame
+    // ----------------------
+    // ranges of particle positions
     // X value: (0.0 - 0.7)
     // Y value: (0.0 - 0.6)
     // Z value: (0.2 - 0.4)
